09f53e3142cb303fb71a18974bb57e33e39aecef
enbraining <me@xolving.com>
2024-01-09 23:09:10 +0900

    asdf

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,12 +9,7 @@ export const defaultLang = 'en';
 export const ui = {
   en: {
     '404.desc': '관련된 문서를 찾을 수 없습니다.',
-    'nav.title': '나의 공간',
-    'nav.note': 'Archive',
-    'nav.note.desc': '필요할 때 꺼내보는',
-    'nav.craft': '공방',
-    'nav.craft.desc': '무언가 흥미로운 것을 만드는 공간',
-    'index.name': '김동학',
+    'index.name': 'enbraining',
     'index.desc':
       `<p><b>숨겨진</b> 요소를 찾는 것이 재미있는 개발자 김동학입니다.</p>` +
       `<p>남들과 소통하며 얻는 <b>새로운</b> 지식을 경험하고, 공유하며 성장합니다.</p>`,
