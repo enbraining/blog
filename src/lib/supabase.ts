0c2c4440e999525c7f25c40c3461d5f858e6b828
enbraining <me@xolving.com>
2024-03-11 23:21:51 +0900

    update page

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.SUPABASE_URL ?? '', process.env.SUPABASE_ANON_KEY ?? '');
+const supabase = createClient(process.env.SUPABASE_URL ?? 'https://xuhajoppozfuonsobfgs.supabase.co', process.env.SUPABASE_ANON_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1aGFqb3Bwb3pmdW9uc29iZmdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY0MzI1NzcsImV4cCI6MjAyMjAwODU3N30.izzLZ7pHnm7fW1opzkZRTEODK3tE68mPFXDN4BzmMoc');
 
 export interface Post {
   id: string;
