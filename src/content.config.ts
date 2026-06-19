1729d997ec7641406f2edd0ea7df7435c51edf7d
enbraining <enbraining@gmail.com>
2026-06-19 23:07:43 +0900

    remove description attribute in blog content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,10 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from 'astro/loaders';
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.{md,mdx}', base: "./src/content" }),
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content" }),
   schema: z.object({
     title: z.string(),
-    description: z.string(),
     date: z.coerce.date(),
     draft: z.boolean().optional(),
     tags: z.array(z.string()).optional(),
